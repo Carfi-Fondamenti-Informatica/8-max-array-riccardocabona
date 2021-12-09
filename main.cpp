2ffee96ec6a9ce27bd168cf23d5d43948a96b0e7
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-09 20:49:50 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim=0;
+    cout<<"inserire dimensione";
+    cin>>dim;
+    float array[dim];
+    cout<<"inserire"<<' '<<dim<<' '<<"numeri";
+    for(int i=0;i<dim;i++){
+        cin>>array[i];
+    }
+    cout<<max(array,dim);
+    return 0;
 }
