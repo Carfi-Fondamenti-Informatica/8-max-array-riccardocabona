175028ae426ba6b1fb3c4e2ab620475a4ccc89d6
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-12-09 20:52:11 +0000

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
+float max(float array[],int dim){
+    float max=array[0];
+    for(int i=1;i<dim;i++){
+        if(array[i]>=max){
+            max=array[i];
+        }
+    }
+    return max;
+}
 
